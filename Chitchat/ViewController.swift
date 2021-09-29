099e7f8bfdb444bcaa83cf47a91f4efd9105df6d
Elvis Wanjohi <elfinalist@gmail.com>
2021-09-29 16:52:52 +0300

    Initial Commit

---
+++ Chitchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chitchat
+//
+//  Created by ELVIS WANJOHI on 29/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

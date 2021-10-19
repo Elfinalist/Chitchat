ff14e2da59b620f48c1c094f19a4bf1f274ade4d
Elvis Wanjohi <elfinalist@gmail.com>
2021-10-19 11:27:39 +0300

    Finalize profile picker integration

--- Chitchat/Controllers/RegisterViewController.swift
+++ Chitchat/Controllers/RegisterViewController.swift
@@ -21,6 +21,9 @@ class RegisterViewController: UIViewController {
         imageView.image = UIImage(systemName: "person")
         imageView.tintColor = .gray
         imageView.contentMode = .scaleAspectFit
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.lightGray.cgColor  
         return imageView
     }()
     
@@ -150,6 +153,9 @@ class RegisterViewController: UIViewController {
                                  y: 20,
                                  width: size,
                                  height: size)
+        
+        imageView.layer.cornerRadius = imageView.width/2.0
+        
         firstnameField.frame = CGRect(x: 30,
                                   y: imageView.bottom+10,
                                   width: scrollView.width-60,
@@ -229,7 +235,7 @@ extension RegisterViewController: UITextFieldDelegate {
     }
 }
 
-extension RegisterViewController:UIImagePickerControllerDelegate {
+extension RegisterViewController:UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func presentPhotoActionSheet() {
         
@@ -241,22 +247,47 @@ extension RegisterViewController:UIImagePickerControllerDelegate {
                                             handler: nil))
         actionSheet.addAction(UIAlertAction(title: "Take Photo",
                                             style: .default,
-                                            handler: { _ in
+                                            handler: { [weak self] _ in
+                                                
+                                                self?.presentCamera()
                                             }))
         actionSheet.addAction(UIAlertAction(title: "Choose Photo",
                                             style: .default,
-                                            handler: { _ in
+                                            handler: { [weak self] _ in
+                                                self?.presentPhotoPicker()
                                             }))
         
         present(actionSheet, animated: true)
     }
     
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    func presentPhotoPicker (){
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+         print(info)
         
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        self.imageView.image = selectedImage
     }
     
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        
+        picker.dismiss(animated: true, completion: nil)
     }
 }
